e100e04fd792322086e4c21da4dfb7b3f7be026b
2487453 <2487453@LTIN665633.cts.com>
2026-05-19 14:02:20 +0530

    5 testcases passed

--- src/main/java/Pages/TravelInsurancePage.java
+++ src/main/java/Pages/TravelInsurancePage.java
@@ -8,6 +8,21 @@ public class TravelInsurancePage {
 
 	WebDriver driver;
 	By clickTravelInsurance = By.xpath("//i[@class=\"icon-bg homeIconsBg icon-bg-new ti\"]");
+	By clickOnUSA = By.xpath("//img[@alt='USA_Image']");
+	By clickOnDate = By.xpath("//div[@class=\"newPq_duration_wrap__dateCol\"][1]//span");
+	By clickOnStartDate = By.xpath("//span[text()='19']");
+	By clickOnEndDate = By.xpath("//span[text()='31']");
+	By clickOnAddTraveller = By.xpath("//a[text()='Add']");
+	By clickOnTwo = By.xpath("//div[@class='memSelectRadioWrapper__radio'][2]");
+	By clickOnDropDown1 = By.xpath("//div[@class='inputRow select '][1]");
+	By selectAge1 = By.xpath("//label[text()='22 years']");
+	By clickOnDropDown2 = By.xpath("//div[@class='inputRow select '][2]");
+	By selectAge2 = By.xpath("//label[text()='21 years']");
+	By checkhealth = By.xpath("//label[text()='No']");
+	By clickOnSubmit = By.xpath("//button[text()='Continue']");
+	By clickDone = By.xpath("//*[@id=\"modal-root\"]/section/article/div/div/div[2]/div[3]/div/button");
+	By clickDone2 = By.xpath("//*[@id=\"modal-root\"]/section/article/div/div/div/div[3]/div/button");
+	By clickOnExplore = By.xpath("//button[@class='travel_main_cta']");
 	public TravelInsurancePage(WebDriver driver) {
 		this.driver=driver;
 	}
@@ -15,7 +30,64 @@ public class TravelInsurancePage {
 		WebElement TravelInsurance= driver.findElement(clickTravelInsurance);
 		return TravelInsurance;
 	}
-	
-	
-	
+	public WebElement chooseDestination() {
+		WebElement Destination= driver.findElement(clickOnUSA);
+		return Destination;
+	}
+	public WebElement chooseDate() {
+		WebElement Date= driver.findElement(clickOnDate);
+		return Date;
+	}
+	public WebElement chooseStartDate() {
+		WebElement StartDate= driver.findElement(clickOnStartDate);
+		return StartDate;
+	}
+	public WebElement chooseEndDate() {
+		WebElement EndDate= driver.findElement(clickOnEndDate);
+		return EndDate;
+	}
+	public WebElement addTravellers() {
+		WebElement AddTraveller= driver.findElement(clickOnAddTraveller);
+		return AddTraveller;
+	}
+	public WebElement selectTwo() {
+		WebElement SelectTwo= driver.findElement(clickOnTwo);
+		return SelectTwo;
+	}
+	public WebElement firstAge() {
+		WebElement FirstAge= driver.findElement(clickOnDropDown1);
+		return FirstAge;
+	}
+	public WebElement selctFirstAge() {
+		WebElement SelectFirstAge= driver.findElement(selectAge1);
+		return SelectFirstAge;
+	}
+	public WebElement secondAge() {
+		WebElement SecondAge= driver.findElement(clickOnDropDown2);
+		return SecondAge;
+	}
+	public WebElement selctSecondAge() {
+		WebElement SelectSecondAge= driver.findElement(selectAge2);
+		return SelectSecondAge;
+	}
+	public WebElement healthCondition() {
+		WebElement HealthCondition= driver.findElement(checkhealth);
+		return HealthCondition;
+	}
+	public WebElement submitAge() {
+		WebElement SubmitAge= driver.findElement(clickOnSubmit);
+		return SubmitAge;
+	}
+	public WebElement done() {
+		WebElement Done= driver.findElement(clickDone);
+		return Done;
+	}
+	public WebElement done2() {
+		WebElement Done2= driver.findElement(clickDone2);
+		return Done2;
+	}
+	public WebElement explorePlans() {
+		WebElement ExplorePlans= driver.findElement(clickOnExplore);
+		return ExplorePlans;
+	}
 }
--- src/test/java/Tests/TravelInsuranceTest.java
+++ src/test/java/Tests/TravelInsuranceTest.java
@@ -1,9 +1,14 @@
 package Tests;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Scanner;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -32,23 +37,78 @@ public class TravelInsuranceTest {
 		
 		driver.get(or.getBaseUrl());
 		sc.close();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
 		
 		
 	}
 	
-	@Test
-	public void getUrl() {
-		System.out.println("Openend");
-	}
+	
 	@Test
 	public void navigateToTravelInsurance() throws InterruptedException {
 		ti=new TravelInsurancePage(driver);
 		ti.clickOnTravelInsurance().click();
 		Thread.sleep(3000);
+
 	}
+	@Test(dependsOnMethods= {"navigateToTravelInsurance"})
+	public void chooseDestination() throws InterruptedException {
+		ti=new TravelInsurancePage(driver);
+		ti.chooseDestination().click();
+		Thread.sleep(3000);
+	}
+	@Test(dependsOnMethods= {"chooseDestination"})
+	public void chooseStartDate() throws InterruptedException {
+		ti=new TravelInsurancePage(driver);
+		ti.chooseDate().click();
+		Thread.sleep(3000);
+		ti.chooseStartDate().click();
+		Thread.sleep(3000);
+		ti.chooseEndDate().click();
+		Thread.sleep(3000);
+		ti.done().click();
+		Thread.sleep(3000);
+
+		
+
+
+
+	}
+	@Test(dependsOnMethods= {"chooseStartDate"})
+	public void addTravellers() throws InterruptedException {
+		ti=new TravelInsurancePage(driver);
+		//ti.addTravellers().click();
+		//Thread.sleep(3000);
+		ti.selectTwo().click();
+		Thread.sleep(3000);
+	}
+	@Test(dependsOnMethods= {"addTravellers"})
+	public void selectAge() throws InterruptedException {
+		ti=new TravelInsurancePage(driver);
+		ti.firstAge().click();
+		ti.selctFirstAge().click();
+		ti.secondAge().click();
+		ti.selctSecondAge().click();
+		ti.healthCondition().click();
+		//ti.submitAge().click();
+		//Thread.sleep(3000);
+		ti.done2().click();
+		Thread.sleep(3000);
+
+		
+	}
+	
+	@Test(dependsOnMethods= {"selectAge"})
+	public void explorePages() throws InterruptedException {
+		ti = new TravelInsurancePage(driver);
+		ti.explorePlans().click();
+		Thread.sleep(3000);
+	}
+	
 	
 	@AfterTest
 	public void tearDown() {
+		if(driver!=null)
 		driver.quit();
 		
 	}
